234bb491e4279ce5d273476e4b0e21472b8ee517
Luciano Mammino <lucianomammino@gmail.com>
2017-03-28 23:49:17 +0100

    Support https in node 4.3.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 /* eslint consistent-return: "off"*/
 
 import { parse } from 'url';
-import { request } from 'http';
+import { request as httpReq } from 'http';
+import { request as httpsReq } from 'https';
 
 export const tall = url => new Promise((resolve, reject) => {
   const { protocol, host, path } = parse(url);
@@ -10,6 +11,7 @@ export const tall = url => new Promise((resolve, reject) => {
   }
 
   const method = 'GET';
+  const request = protocol === 'https:' ? httpsReq : httpReq;
   return request(
     { method, protocol, host, path }, response => resolve(response.headers.location || url),
   ).end();
