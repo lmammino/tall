5752957d53e68e47967d1b9476e345a2d71a7fb0
Andreas Heissenberger <andreas@heissenberger.at>
2019-03-21 10:16:18 +0100

    add option to set User-Agent

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -29,3 +29,8 @@ test("it should return the same url if it's not a short url", () => {
     expect(url).toBe('https://www.nodejsdesignpatterns.com/')
   )
 })
+
+test('it should unshorten medium links / set User-Agent header', async () => {
+  const url = await tall('https://link.medium.com/yNuAPxmdNR', { maxRedirects: 4, headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36' } })
+  expect(url).toMatch(/^https:\/\/hackernoon.com\/everything-i-knew-about-reading-was-wrong-bde7e57fbfdc/)
+})
