661cc63ad17399d52f9aee9b34489ed51962fe81
Luciano <lucianomammino@gmail.com>
2019-03-21 23:22:27 +0100

    version bump and stylistic uniformity

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -34,7 +34,14 @@ test('it should return the same url if it\'s not a short url', (done) => {
   })
 })
 
-test('it should unshorten medium links / set User-Agent header', async () => {
-  const url = await tall('https://link.medium.com/yNuAPxmdNR', { maxRedirects: 4, headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36' } })
-  expect(url).toMatch(/^https:\/\/hackernoon.com\/everything-i-knew-about-reading-was-wrong-bde7e57fbfdc/)
+test('it should unshorten medium links / set User-Agent header', (done) => {
+  tall('https://link.medium.com/yNuAPxmdNR', {
+    maxRedirects: 4,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
+    }
+  }).then(url => {
+    expect(url).toMatch(/^https:\/\/hackernoon.com\/everything-i-knew-about-reading-was-wrong-bde7e57fbfdc/)
+    done()
+  })
 })
