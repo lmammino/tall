456744863458f027d3916e597f78d72c18d2e908
Luciano <lucianomammino@gmail.com>
2021-01-31 18:09:54 +0000

    Completed tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -43,7 +43,64 @@ test('it should fail if a URL without protocol is given', async () => {
   await expect(() => tall('example.com')).rejects.toMatchObject({ message: 'Invalid URL: example.com' })
 })
 
-// TODO: add test for max redirects
-// TODO: add a test for a different method
-// TODO: add a test for sending headers
-// TODO: add a test for no redirect
+test('it should return the original url if no redirect', async () => {
+  nock('https://example.com')
+    .get('/test')
+    .times(1)
+    .reply(200, 'OK')
+
+  const url = await tall('https://example.com/test')
+
+  expect(url).toBe('https://example.com/test')
+})
+
+test('it should follow redirects with relative path', async () => {
+  nock('https://example.com')
+    .get('/test')
+    .times(1)
+    .reply(301, 'Moved', { location: '/test2' })
+    .get('/test2')
+    .times(1)
+    .reply(200, 'OK')
+
+  const url = await tall('https://example.com/test')
+
+  expect(url).toBe('https://example.com/test2')
+})
+
+test('it should respect the maxRedirects option', async () => {
+  nock('https://example.com')
+    .get('/go1')
+    .times(1)
+    .reply(301, 'Moved', { location: 'https://example.com/go2' }) // first redirect
+    .get('/go2')
+    .times(1)
+    .reply(301, 'Moved', { location: 'https://example.com/go3' }) // second redirect <-- should stop here with maxRedirects = 2
+    .get('/go3')
+    .times(1)
+    .reply(301, 'Moved', { location: 'https://example.com/go4' }) // third redirect
+    .get('/go4')
+    .times(1)
+    .reply(301, 'Moved', { location: 'https://example.com/final' }) // fourth redirect
+    .get('/final')
+    .times(1)
+    .reply(200, 'OK')
+
+  const url = await tall('https://example.com/go1', { maxRedirects: 2 })
+
+  expect(url).toBe('https://example.com/go3')
+})
+
+test('it should allow to use a different method', async () => {
+  nock('https://example.com')
+    .head('/test')
+    .times(1)
+    .reply(301, 'Moved', { location: 'https://example.com/test2' })
+    .head('/test2')
+    .times(1)
+    .reply(200, 'OK')
+
+  const url = await tall('https://example.com/test', { method: 'HEAD' })
+
+  expect(url).toBe('https://example.com/test2')
+})
