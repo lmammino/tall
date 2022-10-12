051fe054f0c03ca59f32c854142845947f85383d
Karl Horky <karl.horky@gmail.com>
2022-10-12 20:06:43 +0200

    Disable ESLint rule conflicting with Prettier (#55)
    * Move multi-workspace config to root
    
    * Move outDir to package tsconfig.json
    
    * Add Prettier ESLint config to disable rules
    
    * Add Prettier scripts, format all files
    
    * Ignore lib folders, format file

--- examples/metarefresh-plugin.ts
+++ examples/metarefresh-plugin.ts
@@ -1,7 +1,11 @@
 import { IncomingMessage } from 'http'
 import { Follow, Stop } from 'tall'
 
-export async function metaRefreshPlugin (url: URL, response: IncomingMessage, previous: Follow | Stop): Promise<Follow | Stop> {
+export async function metaRefreshPlugin(
+  url: URL,
+  response: IncomingMessage,
+  previous: Follow | Stop
+): Promise<Follow | Stop> {
   let html = ''
   for await (const chunk of response) {
     html += chunk.toString()
@@ -9,7 +13,9 @@ export async function metaRefreshPlugin (url: URL, response: IncomingMessage, pr
 
   // note: this is actually not a great idea, it's best to use something like cheerio to parse HTML
   // but for the sake of illustrating how to use plugins this is good enough here...
-  const metaHttpEquivUrl = html.match(/meta +http-equiv="refresh" +content="\d;url=(http[^"]+)"/)?.[1]
+  const metaHttpEquivUrl = html.match(
+    /meta +http-equiv="refresh" +content="\d;url=(http[^"]+)"/
+  )?.[1]
 
   if (metaHttpEquivUrl) {
     return new Follow(new URL(metaHttpEquivUrl))
