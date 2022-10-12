abc9c2f0c376d8deb439340b5a02bbf15ae4c875
Karl Horky <karl.horky@gmail.com>
2022-10-12 22:58:53 +0200

    Import types from relative path

--- packages/tall-plugin-meta-refresh/src/index.ts
+++ packages/tall-plugin-meta-refresh/src/index.ts
@@ -1,6 +1,6 @@
 import * as htmlparser2 from 'htmlparser2'
 import { IncomingMessage } from 'http'
-import { Follow, Stop } from 'tall'
+import type { Follow, Stop } from '../../tall'
 
 export async function metaRefreshPlugin(
   url: URL,
